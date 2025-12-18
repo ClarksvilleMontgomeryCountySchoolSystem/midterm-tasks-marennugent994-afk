844795bf4ff060e3ec5d2fff8a680d9f9f186385
marennugent994-afk <maren.nugent994@students.cmcss.net>
2025-12-18 15:29:15 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,32 @@
+# Given variables
+allowance = 20
+dishes, room, trash, lawn, laundry, vacuum = 4, 6, 3, 10, 5, 7
+candy, soda, game, movie, toy, snack = 5, 3, 18, 12, 8, 4
 
+"""
+CHORE MENU - Earn Money:        PURCHASE MENU - Spend Money:
+dishes = $4                      candy = $5
+room = $6                        soda = $3
+trash = $3                       game = $18
+lawn = $10                       movie = $12
+laundry = $5                     toy = $8
+vacuum = $7                      snack = $4
+"""
+
+#Do Not use Ctrl+A to select code. Only copy the code below this line.
+#------------------------------------------------------------------------------------------------
+# Week 1: Chores and first purchase
+# TODO Did Dishes
+# TODO Mowed lawn
+# TODO Bought candy
+
+# Week 2: Bonus week and purchase
+# TODO Bonus (Check description)
+# TODO Vacuumed
+# TODO Bought toy
+
+# Week 3: Savings
+# TODO Saving deposit (Check description)
+
+# Print final allowance
+# TODO FIRST- Allowance print statement. Use given format.
